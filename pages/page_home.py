fd11d632b429da790a405431eadc018e7e451595
liuvgg <2627538472@qq.com>
2025-04-18 16:23:30 +0800

    重置

--- pages/page_home.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-
-class HomePage(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("欢迎来到主页"))
-        self.setLayout(layout)
--- pages/page_settings.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-
-class SettingsPage(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("这里是设置页"))
-        self.setLayout(layout)
